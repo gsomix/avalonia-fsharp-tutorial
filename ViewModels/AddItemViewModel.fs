1998272991c0c33eadf0d8fb3448441cedc8ca3d
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2020-06-20 12:52:51 +0400

    Move Ok command init to constructor

--- ViewModels/AddItemViewModel.fs
+++ ViewModels/AddItemViewModel.fs
@@ -5,26 +5,22 @@ open ReactiveUI
 
 open Todo.Models
 
-type AddItemViewModel() =
+type AddItemViewModel() as this =
     inherit ViewModelBase()
     
     let mutable description = String.Empty
-    let mutable ok = null
     
     member this.Description
         with get() = description
         and set(value) = this.RaiseAndSetIfChanged(&description, value) |> ignore
     
-    member this.Ok =
-        if isNull ok then
-            ok <-
-                ReactiveCommand.Create<_>(
-                    (fun () -> { TodoItem.defaultItem with Description = this.Description }),
-                    this.WhenAnyValue(
-                        (fun x -> x.Description),
-                        (fun x -> String.IsNullOrWhiteSpace(x) |> not)
-                    )
-                )       
-        ok
+    member val Ok =
+        ReactiveCommand.Create<_>(
+            (fun () -> { TodoItem.defaultItem with Description = this.Description }),
+            this.WhenAnyValue(
+                (fun x -> x.Description),
+                (fun x -> String.IsNullOrWhiteSpace(x) |> not)
+            )
+        )       
         
      member val Cancel = ReactiveCommand.Create(fun () -> ())
