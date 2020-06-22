98553ea61ef8e9b4fe17fa7b6fa18f9ebe4c9bc8
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2020-06-22 20:20:18 +0400

    Add lazy for Ok command init

--- ViewModels/AddItemViewModel.fs
+++ ViewModels/AddItemViewModel.fs
@@ -5,26 +5,25 @@ open ReactiveUI
 
 open Todo.Models
 
-type AddItemViewModel() =
+type AddItemViewModel() as this =
     inherit ViewModelBase()
     
     let mutable description = String.Empty
-    let mutable ok = null
+    
+    let ok = lazy (
+        ReactiveCommand.Create<_>(
+            (fun () -> { TodoItem.defaultItem with Description = this.Description }),
+            this.WhenAnyValue(
+                (fun x -> x.Description),
+                (fun x -> String.IsNullOrWhiteSpace(x) |> not)
+            )
+        )   
+    )
     
     member this.Description
         with get() = description
         and set(value) = this.RaiseAndSetIfChanged(&description, value) |> ignore
     
-    member this.Ok =
-        if isNull ok then
-            ok <-
-                ReactiveCommand.Create<_>(
-                    (fun () -> { TodoItem.defaultItem with Description = this.Description }),
-                    this.WhenAnyValue(
-                        (fun x -> x.Description),
-                        (fun x -> String.IsNullOrWhiteSpace(x) |> not)
-                    )
-                )       
-        ok
+    member _.Ok = ok.Force()
         
-     member val Cancel = ReactiveCommand.Create(fun () -> ())
+    member val Cancel = ReactiveCommand.Create(fun () -> ())
